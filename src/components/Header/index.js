22e8d5c0f67cd97076780de4dfe122efe4228f8f
DucSkii <ducvietdao@live.co.uk>
2020-11-10 16:15:28 +0000

    added login/sign up styles and breakpoints for small screens

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,54 @@
 import React from 'react'
-import { Paper, Avatar, IconButton } from '@material-ui/core'
+import { Paper, Avatar, IconButton, Button, makeStyles } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import './styles.scss'
 
+const useStyles = makeStyles(theme => ({
+  buttonSignUp: {
+    marginLeft: '5px',
+    marginRight: '5px',
+    height: '25px',
+    fontSize: '12px',
+    padding: '0',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: '10px',
+      marginRight: '10px',
+      height: '30px',
+      fontSize: '15px',
+      padding: '5px',
+    },
+  },
+  buttonLogin: {
+    height: '25px',
+    padding: '0',
+    [theme.breakpoints.up('sm')]: {
+      height: '30px',
+      fontSize: '15px',
+      padding: '5px',
+    },
+  },
+}))
 
 const Header = () => {
+
+  const classes = useStyles()
 
   return (
     <div className="header">
       <Paper className="header" variant='outlined' square>
         <h3 className='header-logo'>Logo</h3>
-        <div className='header-icons'>
+        <div className='header-login'>
+          <Button color='primary' className={classes.buttonLogin}>Login</Button>
+          <Button variant='outlined' color='secondary' className={classes.buttonSignUp}>Sign-up</Button>
+        </div>
+        {/* <div className='header-icons'>
           <IconButton size='small'>
             <Avatar />
           </IconButton>
           <IconButton size='medium'>
             <MenuIcon />
           </IconButton>
-        </div>
+        </div> */}
       </Paper>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './App';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#ff4081',
-    },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 425,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    }
   }
 })
 
