b68becc9a50f05ffdecfe908ce0bccbea0e75201
DucSkii <ducvietdao@live.co.uk>
2020-11-10 15:45:23 +0000

    setting up firebase posts

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,19 +2,26 @@ import React from 'react'
 import './styles.scss'
 import { Paper, Typography, Avatar } from '@material-ui/core'
 
-const Post = () => {
+const Post = (props) => {
 
   return (
     <div className='post'>
       <header className='post-header'>
         <Paper variant='outlined' square className='post-header-paper' style={{ backgroundColor: 'rgba(245, 0, 87, 0.2)' }}>
           <Avatar className='post-header-avatar' />
-          <Typography variant='h7'>Username</Typography>
+          <Typography variant='h'>{props.username}</Typography>
         </Paper>
       </header>
-      <body>
-        <img className='post-image' src='https://i.pinimg.com/originals/a2/20/98/a220980c4dab3ea9ca2af8d2d1b1a41c.jpg' alt='' />
-      </body>
+      {props.image &&
+        <body>
+          <img className='post-image' src={props.image} alt='' />
+        </body>
+      }
+      <footer className='post-footer'>
+        <Typography>
+          <strong>{props.image ? props.username : ''}</strong> {props.image ? props.caption : <strong>{props.caption}</strong>}
+        </Typography>
+      </footer>
     </div>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase"
+// default snippet, get text from firebase config
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCg3TjoSS4J-ESMwdjulxJZDboyYTSIVtk",
+  authDomain: "social-media-69506.firebaseapp.com",
+  databaseURL: "https://social-media-69506.firebaseio.com",
+  projectId: "social-media-69506",
+  storageBucket: "social-media-69506.appspot.com",
+  messagingSenderId: "736193266324",
+  appId: "1:736193266324:web:6248de2eb8664e96c96a81",
+  measurementId: "G-BYVT1RFCEY"
+})
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const storage = firebase.storage()
+
+export { db, auth, storage }
