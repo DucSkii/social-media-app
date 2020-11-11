f7622b72196aa1284ca4a818fab6a13aeac15264
DucSkii <ducvietdao@live.co.uk>
2020-11-11 12:46:36 +0000

    added condition to make slideshow if image is an array

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,26 +1,74 @@
 import React from 'react'
 import './styles.scss'
-import { Paper, Typography, Avatar } from '@material-ui/core'
+import { Paper, Typography, Avatar, makeStyles } from '@material-ui/core'
+import 'react-slideshow-image/dist/styles.css'
+import { Fade } from "react-slideshow-image";
+
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    width: '23px',
+    height: '23px',
+    marginRight: '5px',
+    [theme.breakpoints.up('sm')]: {
+      width: '26px',
+      height: '26px',
+      marginRight: '7px',
+    },
+  },
+}))
 
 const Post = (props) => {
+
+  const classes = useStyles()
+
+  const renderImage = () => {
+    if (props.image) {
+      if (Array.isArray(props.image)) {
+        return (
+          <Fade
+            autoplay={false}
+            transitionDuration={200}
+          >
+            {
+              props.image.map(image => {
+                return (
+                  <div>
+                    <img src={image} alt='' className='post-image' />
+                  </div>
+                )
+              })
+            }
+          </Fade>
+        )
+      } else {
+        return <img src={props.image} alt='' className='post-image' />
+      }
+    } else {
+      return null
+    }
+  }
 
   return (
     <div className='post'>
       <header className='post-header'>
         <Paper variant='outlined' square className='post-header-paper' style={{ backgroundColor: 'rgba(245, 0, 87, 0.2)' }}>
-          <Avatar className='post-header-avatar' style={{ width: '25px', height: '25px' }} />
-          <Typography style={{ fontSize: '13px' }}>{props.username}</Typography>
+          <Avatar className={classes.avatar} />
+          <Typography style={{ fontSize: '12px' }}><strong>{props.username}</strong></Typography>
         </Paper>
       </header>
-      {props.image &&
-        <div>
-          <img className='post-image' src={props.image} alt='' />
-        </div>
-      }
+      <div>
+        {renderImage()}
+      </div>
       <footer className='post-footer'>
-        <Typography style={{ fontSize: '13px' }}>
-          <strong>{props.image ? props.username : ''}</strong> {props.image ? props.caption : <strong>{props.caption}</strong>}
-        </Typography>
+        {props.image ? (
+          <Typography style={{ fontSize: '12px' }}>
+            <strong>{props.image ? props.username : ''}</strong> {props.image ? props.caption : <strong>{props.caption}</strong>}
+          </Typography>
+        ) : (
+            <Typography style={{ fontSize: '14px' }}>
+              <strong>{props.image ? props.username : ''}</strong> {props.image ? props.caption : <strong>{props.caption}</strong>}
+            </Typography>
+          )}
       </footer>
     </div>
   )
