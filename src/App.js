37fe9493225d51515bc4836621e73ac275cd7571
DucSkii <ducvietdao@live.co.uk>
2020-11-11 21:21:22 +0000

    moved theme provider

--- src/App.js
+++ src/App.js
@@ -7,32 +7,47 @@ import DrawerBar from './components/Drawer'
 import { useGeneralValue } from './context/GeneralContext'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ScrollToTop from './utils/ScrollToTop'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import './App.scss'
+
+const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 425,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    }
+  }
+})
 
 const App = () => {
 
   const [{ isDrawerOpen }, dispatch] = useGeneralValue()
 
   return (
-    <div className='App'>
-      <Router>
-        <ScrollToTop />
-        <Drawer
-          open={isDrawerOpen}
-          anchor='right'
-          onClose={() => dispatch({ type: 'DRAWER_TOGGLE', open: false })}
-        >
-          <DrawerBar />
-        </Drawer>
-        <header>
-          <Header />
-        </header>
-        <Switch>
-          <Route path='/profile' component={Profile} />
-          <Route exact path='/' component={Home} />
-        </Switch>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Router>
+          <ScrollToTop />
+          <Drawer
+            open={isDrawerOpen}
+            anchor='right'
+            onClose={() => dispatch({ type: 'DRAWER_TOGGLE', open: false })}
+          >
+            <DrawerBar />
+          </Drawer>
+          <header>
+            <Header />
+          </header>
+          <Switch>
+            <Route path='/profile' component={Profile} />
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,30 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import { UserContextProvider } from './context/UserContext'
 import { GeneralContextProvider } from './context/GeneralContext'
 import userReducer, { initialUserState } from './reducer/userReducer'
 import generalReducer, { initialState } from './reducer/generalReducer'
 
-const theme = createMuiTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 425,
-      md: 960,
-      lg: 1280,
-      xl: 1920,
-    }
-  }
-})
-
 ReactDOM.render(
   <GeneralContextProvider reducer={generalReducer} initialState={initialState}>
     <UserContextProvider reducer={userReducer} initialState={initialUserState}>
-      <ThemeProvider theme={theme}>
         <App />
-      </ThemeProvider>
     </UserContextProvider>
   </GeneralContextProvider>
   , document.getElementById('root')
