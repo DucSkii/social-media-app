d9060f18e902dd5a7c3e3f8bdc9b5393fa943887
DucSkii <ducvietdao@live.co.uk>
2020-11-15 20:34:44 +0000

    progress on changing profile picture

--- src/pages/Profile/EditProfile/index.js
+++ src/pages/Profile/EditProfile/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useGeneralValue } from '../../../context/GeneralContext'
-import { Paper } from '@material-ui/core'
+import { Paper, Grid, Typography, Input, Avatar, Button } from '@material-ui/core'
 
 import { useStyles } from './styles'
 
@@ -8,11 +8,50 @@ const EditProfile = () => {
 
   const classes = useStyles()
   const [{ darkMode }, dispatch] = useGeneralValue()
+  const [image, setImage] = useState(null)
 
+  const handleChange = e => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0])
+    }
+  }
+  console.log('image', image)
   return (
     <div className={classes.profile}>
       <Paper square className={classes.paper} style={{ backgroundColor: darkMode ? '#666' : '#fafafa' }}>
-        <h1>Edit Profile</h1>
+        <Grid container style={{ paddingTop: '20px' }}>
+          <Grid container item xs={12}>
+            <Grid item xs={1} />
+            <Grid item xs={2}>
+              <Avatar className={classes.avatar} />
+            </Grid>
+            <Grid item xs={1} />
+            <Grid container item xs={7}>
+              <Grid container item xs={12} style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Button variant='outlined' component='label' className={classes.changeButton}>
+                  Change Picture
+                <input type='file' onChange={handleChange} hidden />
+                </Button>
+              </Grid>
+              {
+                image &&
+                <>
+                  <Grid item xs={12} style={{ height: '15px' }} />
+                  <Grid container item xs={12} style={{ justifyContent: 'space-around' }}>
+                    <Grid container item xs={5} style={{ justifyContent: 'center' }}>
+                      <Button variant='outlined' className={classes.button} onClick={() => setImage(null)}>Cancel</Button>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                    <Grid container item xs={5} style={{ justifyContent: 'center' }}>
+                      <Button variant='outlined' className={classes.button}>Confirm</Button>
+                    </Grid>
+                  </Grid>
+                </>
+              }
+            </Grid>
+            <Grid item xs={1} />
+          </Grid>
+        </Grid>
       </Paper>
     </div>
   )
--- src/pages/Profile/EditProfile/styles.js
+++ src/pages/Profile/EditProfile/styles.js
@@ -1,4 +1,4 @@
-import {makeStyles} from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles(theme => ({
   profile: {
@@ -8,5 +8,18 @@ export const useStyles = makeStyles(theme => ({
   paper: {
     height: '100vh',
     width: '100%',
+  },
+  avatar: {
+    height: '70px',
+    width: '70px',
+  },
+  button: {
+    padding: '0px 5px',
+    margin: '0',
+  },
+  changeButton: {
+    fontSize: '12px',
+    minWidth: '170px',
+    height: '30px',
   },
 }))
